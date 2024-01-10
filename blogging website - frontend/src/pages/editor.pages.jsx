7c3d433e83bbfc58fac8e048ee8949a403e92290
ankit gupta <ankitgupta17878@gmail.com>
2024-01-10 21:12:24 +0530

    start BlogEditor Component

--- blogging website - frontend/src/pages/editor.pages.jsx
+++ blogging website - frontend/src/pages/editor.pages.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React ,{useContext,useState} from 'react'
+import { UserContext, } from '../App';
+import { Navigate } from 'react-router-dom';
+import BlogEditor from '../components/blog-editor.component';
+import PublishForm from '../components/publish-form.component';
 
-const editor = () => {
+const Editor = () => {
+
+   const [editorState,setEditorState]=useState('editor');
+  
+   let {userAuth :{access_token}}=useContext(UserContext);
+
   return (
-    <div>editor</div>
+   access_token===null ? <Navigate to="/signin"/> 
+   : editorState==='editor'?<BlogEditor/>:<PublishForm/>
   )
 }
 
-export default editor;
+export default Editor;
