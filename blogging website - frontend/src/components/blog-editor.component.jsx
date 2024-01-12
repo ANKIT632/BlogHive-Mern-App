7ebc47cfab9678ee89003c41364b89ab46f67298
ankit gupta <ankitgupta17878@gmail.com>
2024-01-12 23:20:27 +0530

    work on blog-Editor

--- blogging website - frontend/src/components/blog-editor.component.jsx
+++ blogging website - frontend/src/components/blog-editor.component.jsx
@@ -1,12 +1,18 @@
 import {Link} from 'react-router-dom';
 import logo from '../imgs/logo.png';
 import AnimationWrapper from '../common/page-animation';
+import defaultBannner from '../imgs/blog banner.png';
 
 const BlogEditor= () => {
+
+  const handleBannerUpload=(e)=>{
+        let selectImg=e.target.files[0];
+  }
+
   return (
 
     <>
-         <nav className='navbar'>
+         <nav className='navbar shadow'>
            <Link to='/' className='flex-none w-10'>
              <img src={logo}/>
            </Link>
@@ -30,8 +36,24 @@ const BlogEditor= () => {
 
     <AnimationWrapper>
             <section>
-              <div >
-                
+              <div className='mx-auto max-w-[900px] w-full'>
+                  <div className='relative aspect-video bg-white border-4 border-grey'>
+                      <label htmlFor='uploadBanner'>
+                            <img
+                              src={defaultBannner}
+                              className='z-20'
+                              style={{ cursor: 'pointer' }}
+                            />
+                      </label>
+
+                      <input 
+                        id='uploadBanner'
+                        type='file'
+                        accept='.png, .jpg ,.jpeg'
+                        hidden
+                        onChange={handleBannerUpload}
+                      />
+                  </div>
               </div>
             </section>
            </AnimationWrapper>
@@ -45,3 +67,4 @@ const BlogEditor= () => {
 export default BlogEditor;
 
 //line-clamp-1 means if text length overflow the show dot...
+// How to work click input  ? lable are active click event for input .
